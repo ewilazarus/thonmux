c745d400445b696228548189a27e04169b2d07c6
Gabriel Lima <ewilazarus@gmail.com>
2015-06-07 14:37:41 +0200

    documenting main.py

--- thonmux/main.py
+++ thonmux/main.py
@@ -16,16 +16,15 @@ class Thonmux:
     really exists in "tmux".
 
     It works by keeping track of the *session* provided to its constructor, as
-    well as of to its active *window* and active *pane*. Whenever an action
-    that would alter the tree's state happens, it raises an `exception.Entity
-    OutOfSync` that is handled by reconstructing the tree and updating the
+    well as to its active *window* and active *pane*. Whenever an action that
+    would alter the tree's state happens, it raises an `exception.EntityOutOf
+    Sync`_ that is handled by reconstructing the tree and updating the
     references of the tracked *session*, *window* and *pane*.
 
     All "tmux" commands are run against the tracked *session*, *window* and
     *pane* as needed.
 
     Currently, there's support for the following "tmux" command:
-    - kill-session
     - rename-session
     - new-window
     - select-window
@@ -72,47 +71,119 @@ class Thonmux:
         self.pane = self.window.active_pane
         logger.debug('Synchronizing Thonmux: ' + str(self))
 
-    def kill_session(self, name):
-        self.session.kill()
-
     def rename_session(self, name):
+        """method:: rename_session(name)
+        Renames the tracked *session*.
+
+        :param str name: The name to be applied to the session
+        """
         self.session.rename(name)
 
-    def new_window(self, name, start_dir=None, target=None):
-        self.session.new_window(name, start_dir, target)
+    def new_window(self, name, start_dir=None):
+        """method:: new_window(name, [start_dir=None, target=None])
+        Creates a new window under the tracked *session*. Updates the tracked
+        *session*, *window* and *pane* references.
+
+        :param str name: The name to be applied to the window
+        :param start_dir: The starting directory for the new window
+        :type start_dir: str or None
+        """
+        self.session.new_window(name, start_dir=start_dir)
 
     def select_window(self, index):
+        """method:: select_window(index)
+        Selects the window with the given index under the tracked *session*.
+        Updates the tracked *window* and *pane* references.
+
+        :param str index: The index of the target window
+        """
         self.session.select_window(index)
 
     def next_window(self):
+        """method:: next_window()
+        Selects the next window under the tracked *session*. Updates the
+        tracked *window* and *pane* references.
+        """
         self.session.next_window()
 
     def previous_window(self):
+        """method:: previous_window()
+        Selects the previous window under the tracked *session*. Updates the
+        tracked *window* and *pane* references.
+        """
         self.session.previous_window()
 
     def last_window(self):
+        """method:: last_window()
+        Selects the last window under the tracked *session*. Updates the
+        tracked *window* and *pane* references.
+        """
         self.session.last_window()
 
     def rename_window(self, name):
+        """method:: rename_window(name)
+        Renames the tracked *window*.
+
+        :param str name: The name to be applied to the window
+        """
         self.window.rename(name)
 
     def kill_window(self):
+        """method:: kill_window()
+        Kills (removes) the tracked *window* from under the tracked *session*.
+        Updates the tracked *session*, *window* and *pane* references.
+        """
         self.window.kill()
 
-    def window_split(self, horizontal=False, start_dir=None, target=None):
-        self.window.split(horizontal, start_dir, target)
+    def window_split(self, horizontal=False, start_dir=None):
+        """method:: window_split([horizontal=False, start_dir=None)
+        Splits the tracked *pane*. Updates the tracked *window* and *pane*
+        references.
+
+        :param bool horizontal: Defines whether the split is going to be
+        vertical (False) or horizontal (True)
+        :param start_dir: The starting directory for the new pane
+        :type start_dir: str or None
+        """
+        self.window.split(horizontal, start_dir=start_dir)
 
     def select_pane(self, index):
+        """method:: select_pane(index)
+        Selects the pane with the given index under the tracked *window*.
+        Updates the tracked *pane* reference.
+
+        :param str index: The index of the target pane
+        """
         self.window.select_pane(index)
 
     def kill_pane(self):
+        """method:: kill_pane()
+        Kills (removes) the tracked *pane* from under the tracked *window*
+        Updates the tracked *window* and *pane* references.
+        """
         self.pane.kill()
 
     def resize_pane(self, width, height):
+        """method:: resize_pane(width, height)
+        Resizes the tracked *pane* using the given width and height.
+
+        :param str width: The width to be applied to the target pane
+        :param str height: The height to be applied to the target pane
+        """
         self.pane.resize(width, height)
 
     def send_keys(self, keys, enter=True):
+        """method:: send_keys(keys[, enter=True])
+        Sends the given keys to the tracked *pane*.
+
+        :param str keys: The string to be sent to the target pane
+        :param bool enter: Defines whether an 'Enter' should be sent after the
+        command (True) or not (False)
+        """
         self.pane.send_keys(keys, enter)
 
     def toggle_zoom(self):
+        """method:: toggle_zoom()
+        Zooms in/out of the tracked *pane*.
+        """
         self.pane.zoom()
