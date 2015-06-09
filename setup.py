19784bf5167c2502cb847ce125982cce4cd37299
Gabriel Lima <ewilazarus@gmail.com>
2015-06-09 15:41:08 +0200

    release 0.0.2

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(name='thonmux',
       packages=['thonmux'],
-      version='0.0.1',
+      version='0.0.2',
       description='Interact with tmux in a pythonic way',
       author='Gabriel Lima',
       author_email='ewilazarus@gmail.com',
       url='https://github.com/ewilazarus/thonmux',
-      download_url='https://github.com/ewilazarus/thonmux/tarball/0.0.1',
-      keywords=['tmux'],
+      download_url='https://github.com/ewilazarus/thonmux/tarball/0.0.2',
+      keywords=['tmux', 'api'],
       classifiers=[])
